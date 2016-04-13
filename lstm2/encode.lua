5ac14d79bc6aad0d383505cc6a5f60bbf5e153b6
Alexander <aleks_bs95@abv.bg>
2016-04-14 00:00:28 +0300

    encode is working

--- lstm2/encode.lua
+++ lstm2/encode.lua
@@ -1,10 +1,11 @@
 local json = require 'cjson'
 local MIDI = require 'MIDI'
+local lfs = require 'lfs'
 cmd = torch.CmdLine()
 cmd:option('-filename', 'File to be transformed')
 
 opt = cmd:parse(arg)
-print("ddddLLLLLLLLLLLLLLLL");
+--print(lfs.currentdir())
 local f = assert(io.open(opt.filename, "r"))
 local rawdata = MIDI.midi2score(f:read("*all"))
 print(json.encode(rawdata))
